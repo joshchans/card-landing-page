2abc5eafcc9c21e534d56fc6dae393afcfae72eb
Josh Chan <jfusion3600@gmail.com>
2025-12-31 08:48:52 +1100

    Syntax error fix

--- js/lighting.js
+++ js/lighting.js
@@ -1,11 +1,12 @@
 import * as THREE from "./three.module.js";
 
 export function setupLighting(scene) {
-scene.add(new THREE.AmbientLight(0xffffff, 0.6));
+  scene.add(new THREE.AmbientLight(0xffffff, 0.6));
 
-const hemi = new THREE.HemisphereLight(
-  0xffffff, // sky
-  0x444444, // ground
-  1.0
-);
-scene.add(hemi);
+  const hemi = new THREE.HemisphereLight(
+    0xffffff, // sky
+    0x444444, // ground
+    1.0
+  );
+  scene.add(hemi);
+}
